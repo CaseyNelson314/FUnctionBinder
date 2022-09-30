35802732c95c6117e0d87cfc6594f154f5f584d6
CaseyNelson314 <caseynelson314@gmail.com>
2022-09-30 14:44:12 +0900

    hogehoge commit

---
+++ sketch_sep30a/sketch_sep30a.ino
@@ -0,0 +1,28 @@
+#include "FunctionBinder.h"
+
+#include <Wire.h>
+
+class Reader : FunctionBinder<void(int)>
+{
+	public:
+		Reader() {
+			Wire.onReceive(FunctionBinder<void(int)>::bind);
+		}
+		void callback(int) override {
+			Serial.println("received");
+		}
+};
+
+void setup() {
+	Serial.begin(115200);
+	Wire.begin(0);
+}
+void loop() {
+	/*
+		受信割り込みのためloop内は空
+		received
+		received
+		received
+		received
+	*/
+}
